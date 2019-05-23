9a87c4ea5bd9836ef7553b4edf7a9a867143dee5
Dario Sarmiento <50173164+darioingenierosistemas@users.noreply.github.com>
2019-05-23 17:56:59 -0500

    Orden del Proyecto

---
+++ consulta_Ejecutiva/Actividades/Act_Login.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace consulta_Ejecutiva
+{
+    [Activity(Label = "LOGIN", Theme = "@style/AppTheme"
+        , MainLauncher = true
+        )]
+    public class Act_Login : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Lay_Login);
+        }
+    }
+}

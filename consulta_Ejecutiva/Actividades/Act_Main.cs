0d7e9e46e20094ad1319055c77d816ec51081010
Dario Sarmiento <50173164+darioingenierosistemas@users.noreply.github.com>
2019-05-29 10:18:50 -0500

    Segunda Confirmacion con los cambios de Jose

--- consulta_Ejecutiva/Actividades/Act_Main.cs
+++ consulta_Ejecutiva/Actividades/Act_Main.cs
@@ -43,9 +43,61 @@ namespace consulta_Ejecutiva.Actividades
             FloatingActionButton btnLineChart = FindViewById<FloatingActionButton>(Resource.Id.fabLineChart);
             btnLineChart.Click += BtnLineChart_Click;
 
+            FloatingActionButton BtnDonutChart = FindViewById<FloatingActionButton>(Resource.Id.fabDonutChart);
+            BtnDonutChart.Click += BtnDonutChart_Click;
+
+            Mes1 = FindViewById<CheckBox>(Resource.Id.btnUnMes);
+            Mes1.CheckedChange += Mes1_CheckedChange;
+
+            Meses6 = FindViewById<CheckBox>(Resource.Id.btnSeisMes);
+            Meses6.CheckedChange += Meses6_CheckedChange;
+
+            Anho1 = FindViewById<CheckBox>(Resource.Id.btnUnAnho);
+            Anho1.CheckedChange += Anho1_CheckedChange;
+
         }
 
-        private void BtnBarChart_Click(object sender, EventArgs e)
+        private void CheckStatus()
+        {
+            Mes1.Checked = false;
+            Meses6.Checked = false;
+            Anho1.Checked = false;
+        }
+
+        private void Mes1_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (Meses6.Checked || Anho1.Checked)
+            {
+                Meses6.Checked = false;
+                Anho1.Checked = false;
+            }
+        }
+
+        private void Meses6_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (Mes1.Checked || Anho1.Checked)
+            {
+                Mes1.Checked = false;
+                Anho1.Checked = false;
+            }
+        }
+
+        private void Anho1_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (Meses6.Checked || Mes1.Checked)
+            {
+                Meses6.Checked = false;
+                Mes1.Checked = false;
+            }
+        }
+
+        private void BtnDonutChart_Click(object sender, EventArgs e)
+        {
+            var DonutChart_ = new Intent(this, typeof(Act_DonutChart));
+            StartActivity(DonutChart_);
+        }
+
+            private void BtnBarChart_Click(object sender, EventArgs e)
 		{
 			var intent = new Intent(this, typeof(Act_Grafico_BarChart));
 			StartActivity(intent);

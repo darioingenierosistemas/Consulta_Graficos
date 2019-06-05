126724f69ef497bb58e3ee5fb4b929ef124cf16e
davidmontoyaheredia <davidmontoyaheredia@hotmail.com>
2019-06-05 15:57:13 -0500

    Se hizo que las gráficas se crearan de forma dinámica.

--- consulta_Ejecutiva/Actividades/Act_LineChart.cs
+++ consulta_Ejecutiva/Actividades/Act_LineChart.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -20,7 +21,9 @@ namespace consulta_Ejecutiva.Actividades
     public class Act_LineChart : Activity
     {
         private string codContratista;
+        private string nomContratista;
         private string mes;
+        private int anho;
         ObservableCollection<ChartData> Data = new ObservableCollection<ChartData>();
         ObservableCollection<ChartData> Data2 = new ObservableCollection<ChartData>();
 
@@ -29,6 +32,7 @@ namespace consulta_Ejecutiva.Actividades
             base.OnCreate(savedInstanceState);
 
             codContratista = Intent.GetStringExtra("Contratista");
+            nomContratista = Intent.GetStringExtra("NomContratista");
             mes = Intent.GetStringExtra("Mes");
 
             GetData();
@@ -38,59 +42,97 @@ namespace consulta_Ejecutiva.Actividades
         {
             try
             {
-                string url = URLs.ConMes1 + "1245" + URLs.ConMes1y1 + mes;
-                var resultado = await url.GetRequest<List<TABLA_MES1>>();
-
-                SfChart chart = new SfChart(this);
-
-                CategoryAxis primaryAxis = new CategoryAxis();
-                primaryAxis.Title.Text = "Semanas";
-                chart.PrimaryAxis = primaryAxis;
-                chart.PrimaryAxis.EdgeLabelsDrawingMode = EdgeLabelsDrawingMode.Shift;
-                chart.PrimaryAxis.LabelsIntersectAction = AxisLabelsIntersectAction.MultipleRows;
-
-                NumericalAxis secondaryAxis = new NumericalAxis();
-                secondaryAxis.Title.Text = "Longitud";
-                chart.SecondaryAxis = secondaryAxis;
-
-                var meses = Convert.ToInt16(mes);
-                for (int i = 0; i < meses; i++)
+                if (mes == "4")
                 {
-                    Data.Add(new ChartData { Semanas = "Semana " + resultado[i].SEMANA, LongPatrullada = resultado[i].LONGITUD_PATRULLADA });
+                    string url = URLs.ConMes1 + codContratista + URLs.ConMes1y1 + mes;
+                    var resultado = await url.GetRequest<List<TABLA_MES1>>();
 
-                    Data2.Add(new ChartData { Semanas = "Semana " + resultado[i].SEMANA, LongPatrullada = resultado[i].LONGITUD_ASIGNADA });
+                    foreach (var datos in resultado)
+                    {
+                        Data.Add(new ChartData { Semanas = "Semana " + datos.SEMANA, LongPatrullada = datos.LONGITUD_PATRULLADA });
+                        Data2.Add(new ChartData { Semanas = "Semana " + datos.SEMANA, LongPatrullada = datos.LONGITUD_ASIGNADA });
+                        anho = datos.ANHO;
+                    }
+
+                    CreateLineChart();
                 }
-
-                LineSeries lineSeries = new LineSeries()
+                else if (mes == "6")
                 {
-                    ItemsSource = Data,
-                    XBindingPath = "Semanas",
-                    YBindingPath = "LongPatrullada"
-                };
-                lineSeries.Label = "Longitud Patrullada";
-                lineSeries.DataMarker.ShowLabel = true;
-                lineSeries.TooltipEnabled = true;
+                    string url = URLs.ConMeses6 + codContratista + URLs.ConMeses6y1 + mes;
+                    var resultado = await url.GetRequest<List<TABLA_MESES>>();
 
-                LineSeries lineSeries2 = new LineSeries()
+                    foreach (var datos in resultado)
+                    {
+                        Data.Add(new ChartData { Semanas = "Semana " + datos.MES, LongPatrullada = datos.LONGITUD_PATRULLADA });
+                        Data2.Add(new ChartData { Semanas = "Semana " + datos.MES, LongPatrullada = datos.LONGITUD_ASIGNADA });
+                        anho = datos.ANHO;
+                    }
+
+                    CreateLineChart();
+                }
+                else if (mes == "12")
                 {
-                    ItemsSource = Data2,
-                    XBindingPath = "Semanas",
-                    YBindingPath = "LongPatrullada"
-                };
-                lineSeries2.Label = "Longitud Asignada";
-                lineSeries2.DataMarker.ShowLabel = true;
-                lineSeries2.TooltipEnabled = true;
+                    string url = URLs.ConMeses12 + codContratista + URLs.ConMeses12y1 + mes;
+                    var resultado = await url.GetRequest<List<TABLA_MESES>>();
 
-                chart.Series.Add(lineSeries);
-                chart.Series.Add(lineSeries2);
-                chart.Legend.Visibility = Visibility.Visible;
+                    foreach (var datos in resultado)
+                    {
+                        Data.Add(new ChartData { Semanas = "Semana " + datos.MES, LongPatrullada = datos.LONGITUD_PATRULLADA });
+                        Data2.Add(new ChartData { Semanas = "Semana " + datos.MES, LongPatrullada = datos.LONGITUD_ASIGNADA });
+                        anho = datos.ANHO;
+                    }
 
-                SetContentView(chart);
+                    CreateLineChart();
+                }
             }
             catch (Exception ex)
             {
                 Toast.MakeText(ApplicationContext, ex.ToString(), ToastLength.Long).Show();
             }
+        }
+
+        private void CreateLineChart()
+        {
+            SfChart chart = new SfChart(this);
+            chart.Title.Text = "CONTRATISTA: " + nomContratista;
+            chart.Title.Typeface = Typeface.DefaultBold;
+
+            CategoryAxis primaryAxis = new CategoryAxis();
+            primaryAxis.Title.Text = "Semanas";
+            chart.PrimaryAxis = primaryAxis;
+            chart.PrimaryAxis.EdgeLabelsDrawingMode = EdgeLabelsDrawingMode.Shift;
+            chart.PrimaryAxis.LabelsIntersectAction = AxisLabelsIntersectAction.MultipleRows;
+
+            NumericalAxis secondaryAxis = new NumericalAxis();
+            secondaryAxis.Title.Text = "Longitud (mt)";
+            chart.SecondaryAxis = secondaryAxis;
+
+            LineSeries lineSeries = new LineSeries()
+            {
+                ItemsSource = Data,
+                XBindingPath = "Semanas",
+                YBindingPath = "LongPatrullada"
+            };
+            lineSeries.Label = "Longitud Patrullada";
+            lineSeries.DataMarker.ShowLabel = true;
+            lineSeries.TooltipEnabled = true;
+
+            LineSeries lineSeries2 = new LineSeries()
+            {
+                ItemsSource = Data2,
+                XBindingPath = "Semanas",
+                YBindingPath = "LongPatrullada"
+            };
+            lineSeries2.Label = "Longitud Asignada";
+            lineSeries2.DataMarker.ShowLabel = true;
+            lineSeries2.TooltipEnabled = true;
+
+            chart.Series.Add(lineSeries);
+            chart.Series.Add(lineSeries2);
+            chart.Legend.Title.Text = "Año " + anho;
+            chart.Legend.Visibility = Visibility.Visible;
+
+            SetContentView(chart);
         }
 
         public class ChartData

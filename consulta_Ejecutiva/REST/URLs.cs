bdac09bc92db5b0dc3ce9c07e092ab3cd13b15af
Dario Sarmiento <50173164+darioingenierosistemas@users.noreply.github.com>
2019-06-07 08:55:49 -0500

    Otro cambio URLs

--- consulta_Ejecutiva/REST/URLs.cs
+++ consulta_Ejecutiva/REST/URLs.cs
@@ -17,18 +17,21 @@ namespace consulta_Ejecutiva.REST
         public static string ConDep = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/departamentos/";
         public static string ConUni = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/unidad/?CodDepartamento=";
         public static string ConCon = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/contratista/?CodUnidadOperativa=";
+
         public static string ConMes1 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/ConsultaMes1/?CodContratista=";
         public static string ConMeses6 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/ConsultaMeses6/?CodContratista=";
         public static string ConMeses12 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/ConsultaMeses12/?CodContratista=";
-        public static string ConLonDepMes1 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudAsigDepartamento/?CodDepartamento=";
-        public static string ConLonDepMes6 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudAsigDepartamentoMes/?CodDepartamento=";
-        public static string ConLonUniMes1 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudUnidadSemana/?CodUnidadOperativa=";
+
+        public static string LonDepMes1 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudAsigDepartamento/?CodDepartamento=";
+        public static string LonDepMes6 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudAsigDepartamentoMes/?CodDepartamento=";
+        public static string LonUniMes1 = "http://testlab.g-gis.com:80/Rest_Ejecutivo/ejecutiva/consulta/LongitudUnidadSemana/?CodUnidadOperativa=";
 
         public static string ConMes1y1 = "&mes=";
         public static string ConMeses6y1 = "&meses=";
-        public static string ConMeses12y1 = "&meses="; 
-        public static string ConLonDepMes1y1 = "&semanas=";
-        public static string ConLonDepMes6y1 = "&meses=";
-        public static string ConLonUniMes1y1 = "&semanas=";
+        public static string ConMeses12y1 = "&meses=";
+
+        public static string LonDepMes1y1 = "&semanas=";
+        public static string LonDepMes6y1 = "&meses=";
+        public static string LonUniMes1y1 = "&semanas=";
     }
 }
